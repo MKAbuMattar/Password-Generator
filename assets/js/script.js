8ed041c0c2a5d97f33e388a48d2317847243eea4
Mohammad Abu Mattar <mohammad.khaled@outlook.com>
2020-12-31 23:35:07 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -283,7 +283,7 @@ copyBtn.addEventListener('click', () => {
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
-		navigator.serviceWorker.register('/sw.js').then(function (registration) {
+		navigator.serviceWorker.register('/Password-Generator/sw.js').then(function (registration) {
 			// Registration was successful
 			console.log('ServiceWorker registration successful with scope: ', registration
 				.scope);
