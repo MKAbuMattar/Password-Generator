0b8369c88425325da6dfc53c920ac017ec6decc6
Mohammad Abu Mattar <mohammad.khaled@outlook.com>
2020-12-31 23:33:59 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,132 +1,181 @@
 function app() {
-	const id = document.getElementById("app");
+	let id = document.getElementById("app");
 
-	const box = document.createElement("div");
+	/* floating */
+	let floating = document.createElement("div");
+	floating.classList.add("floating");
+
+	let installAppBtn = document.createElement("a");
+	installAppBtn.setAttribute("class", "floating__btn add-button");
+	installAppBtn.title = "Install App";
+
+	let installAppBtnSVG = document.createElement("svg");
+	installAppBtnSVG.setAttribute("height", "48");
+	installAppBtnSVG.setAttribute("width", "48");
+	installAppBtnSVG.setAttribute("viewBox", "0 0 48 48");
+	installAppBtnSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+	let installAppBtnSVGPath = document.createElement("path");
+	installAppBtnSVGPath.setAttribute("fill-rule", "evenodd");
+	installAppBtnSVGPath.setAttribute("clip-rule", "evenodd");
+	installAppBtnSVGPath.id = "install__app";
+	installAppBtnSVGPath.setAttribute("d", "M8 7C8 4.23858 10.2386 2 13 2H35C37.7614 2 40 4.23858 40 7V41C40 43.7614 37.7614 46 35 46H13C10.2386 46 8 43.7614 8 41V7ZM13 6C12.4477 6 12 6.44772 12 7V41C12 41.5523 12.4477 42 13 42H35C35.5523 42 36 41.5523 36 41V7C36 6.44772 35.5523 6 35 6H13ZM18 10C18 9.44772 18.4477 9 19 9H29C29.5523 9 30 9.44772 30 10V12C30 12.5523 29.5523 13 29 13H19C18.4477 13 18 12.5523 18 12V10ZM24 39C25.6569 39 27 37.6569 27 36C27 34.3431 25.6569 33 24 33C22.3431 33 21 34.3431 21 36C21 37.6569 22.3431 39 24 39Z");
+	/* end floating */
+
+	let box = document.createElement("section");
 	box.classList.add("box");
 
-	const title = document.createElement("div");
+	let title = document.createElement("div");
 	title.classList.add("title");
 
-	const titleH1 = document.createElement("h1");
+	let titleH1 = document.createElement("h1");
 	titleH1.innerHTML = "Password Generator";
 
 	/* screen */
-	const screen = document.createElement("div");
-	screen.classList.add("screen");
+	let resultBox = document.createElement("div");
+	resultBox.classList.add("result__box");
 
-	const output = document.createElement("input");
-	output.type = "text";
-	output.readOnly = "readonly";
-	output.value = "";
-	output.classList.add("output");
+	let result = document.createElement("input");
+	result.type = "text";
+	result.id = "result";
+	result.placeholder = "New Password Generator";
+	result.readOnly = "readonly";
 
-	const btnCopy = document.createElement("button");
+	let btnCopy = document.createElement("button");
 	btnCopy.innerHTML = "Copy";
 	btnCopy.id = "copy";
-	btnCopy.setAttribute("onclick", "copyText()");
 	btnCopy.classList.add("btn");
 	/* end screen */
 
+	/* alert */
+	let alert_box = document.createElement("div");
+	alert_box.classList.add("alert__box");
+
+	let alert = document.createElement("span");
+	alert.classList.add("alert");
+	alert.id = "alert";
+
+	let alertMessage = document.createElement("span");
+	alertMessage.classList.add("message");
+	alertMessage.id = "message";
+	/* end alert */
+
 	/* items */
-	const items = document.createElement("div");
+	let items = document.createElement("div");
 	items.classList.add("items");
 
-	const item1 = document.createElement("div");
+	let item1 = document.createElement("div");
 	item1.classList.add("item");
 
-	const passlengthL = document.createElement("label");
-	passlengthL.for = "passlength";
-	passlengthL.innerHTML = "Password Length :";
+	let passwordLengthL = document.createElement("label");
+	passwordLengthL.for = "password__length";
+	passwordLengthL.innerHTML = "Password Length :";
 
-	const passlengthI = document.createElement("input");
-	passlengthI.type = "number";
-	passlengthI.value = "12";
-	passlengthI.min = "6";
-	passlengthI.id = "passlength";
+	let passwordLengthI = document.createElement("input");
+	passwordLengthI.type = "number";
+	passwordLengthI.value = "20";
+	passwordLengthI.min = "6";
+	passwordLengthI.id = "password__length";
 
-	const item2 = document.createElement("div");
+	let item2 = document.createElement("div");
 	item2.classList.add("item");
 
-	const upercaseL = document.createElement("label");
-	upercaseL.for = "upercase";
-	upercaseL.innerHTML = "Include Upercase :";
+	let uppercaseL = document.createElement("label");
+	uppercaseL.htmlFor = "uppercase";
+	uppercaseL.innerHTML = "Include Uppercase :";
 
-	const upercaseI = document.createElement("input");
-	upercaseI.type = "checkbox";
-	upercaseI.checked = "checked";
-	upercaseI.id = "upercase";
+	let uppercaseI = document.createElement("input");
+	uppercaseI.type = "checkbox";
+	uppercaseI.checked = "checked";
+	uppercaseI.id = "uppercase";
 
-	const item3 = document.createElement("div");
+	let item3 = document.createElement("div");
 	item3.classList.add("item");
 
-	const lowercaseL = document.createElement("label");
-	lowercaseL.for = "lowercase";
+	let lowercaseL = document.createElement("label");
+	lowercaseL.htmlFor = "lowercase";
 	lowercaseL.innerHTML = "Include Lowercase :";
 
-	const lowercaseI = document.createElement("input");
+	let lowercaseI = document.createElement("input");
 	lowercaseI.type = "checkbox";
 	lowercaseI.checked = "checked";
 	lowercaseI.id = "lowercase";
 
-	const item4 = document.createElement("div");
+	let item4 = document.createElement("div");
 	item4.classList.add("item");
 
-	const numbersL = document.createElement("label");
-	numbersL.for = "numbers";
+	let numbersL = document.createElement("label");
+	numbersL.htmlFor = "numbers";
 	numbersL.innerHTML = "Include Numbers :";
 
-	const numbersI = document.createElement("input");
+	let numbersI = document.createElement("input");
 	numbersI.type = "checkbox";
 	numbersI.checked = "checked";
 	numbersI.id = "numbers";
 
-	const item5 = document.createElement("div");
+	let item5 = document.createElement("div");
 	item5.classList.add("item");
 
-	const symbolsL = document.createElement("label");
-	symbolsL.for = "symbols";
+	let symbolsL = document.createElement("label");
+	symbolsL.htmlFor = "symbols";
 	symbolsL.innerHTML = "Include Symbols :";
 
-	const symbolsI = document.createElement("input");
+	let symbolsI = document.createElement("input");
 	symbolsI.type = "checkbox";
 	symbolsI.checked = "checked";
 	symbolsI.id = "symbols";
 
-	const generate = document.createElement("button");
-	generate.innerHTML = "Generate Password";
-	generate.id = "generate";
-	generate.classList.add("btn");
+	let btnBox = document.createElement("div");
+	btnBox.classList.add("btn__box");
 
+	let generateBtn = document.createElement("button");
+	generateBtn.innerHTML = "Generate Password";
+	generateBtn.id = "generate";
+	generateBtn.setAttribute("class", "btn btn__block");
 	/* end items */
 
 	/* footer */
 	let footer = document.createElement("footer");
 	let footerP = document.createElement("p");
-	let footerY = new Date().getFullYear() > 2020 && (" - " + new Date().getFullYear());
+	let footerY = (new Date().getFullYear() > 2020) ? (" - " + new Date().getFullYear()) : ('');
 	let footerA = document.createElement("a");
 	footerA.href = "https://mkabumattar.github.io/";
 	footerA.innerHTML = "Mohammad Khaled Abu Mattar";
 	footerP.innerHTML = "All Copyrights Reserved &#169; 2020" + footerY + ", ❤ Made With  & a lot ☕ By ";
 	/* end footer */
 
+	/******************** layout ********************/
+	/* floating */
+	id.appendChild(floating);
+	floating.appendChild(installAppBtn);
+	installAppBtn.appendChild(installAppBtnSVG);
+	installAppBtnSVG.appendChild(installAppBtnSVGPath);
+	/* end floating */
+
 	id.appendChild(box);
 	box.appendChild(title);
 	title.appendChild(titleH1);
 
-	/* screen */
-	box.appendChild(screen);
-	screen.appendChild(output);
-	screen.appendChild(btnCopy);
-	/* end screen */
+	/* result */
+	box.appendChild(resultBox);
+	resultBox.appendChild(result);
+	resultBox.appendChild(btnCopy);
+	/* end result */
+
+	/* alart */
+	box.appendChild(alert_box);
+	alert_box.appendChild(alert);
+	alert.appendChild(alertMessage);
+	/* end alart */
 
 	/* items */
 	box.appendChild(items);
 	items.appendChild(item1);
-	item1.appendChild(passlengthL);
-	item1.appendChild(passlengthI);
+	item1.appendChild(passwordLengthL);
+	item1.appendChild(passwordLengthI);
 	items.appendChild(item2);
-	item2.appendChild(upercaseL);
-	item2.appendChild(upercaseI);
+	item2.appendChild(uppercaseL);
+	item2.appendChild(uppercaseI);
 	items.appendChild(item3);
 	item3.appendChild(lowercaseL);
 	item3.appendChild(lowercaseI);
@@ -136,7 +185,8 @@ function app() {
 	items.appendChild(item5);
 	item5.appendChild(symbolsL);
 	item5.appendChild(symbolsI);
-	items.appendChild(generate);
+	box.appendChild(btnBox);
+	btnBox.appendChild(generateBtn);
 	/* end items */
 
 	/* footer */
@@ -144,77 +194,143 @@ function app() {
 	footer.appendChild(footerP);
 	footerP.appendChild(footerA);
 	/* end footer */
+	/****************** end layout ******************/
 }
 
 app();
 
+/*******************************/
+const uppercaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZGDJD";
+const lowercaseStr = "abcdefghijklmnopqrstuvwxyzhgsfd";
+const numbersStr = "0123456789635694378265487";
+const symbolstr = "&_)$*%^!=-/<>(@``ç#|@\)=&";
 
+const result = document.getElementById("result");
+const copyBtn = document.getElementById("copy");
+const alert = document.getElementById('alert');
+const alertMessage = document.getElementById('message');
+const passwordLengthInput = document.getElementById("password__length");
+const uppercaseInput = document.getElementById("uppercase");
+const lowercaseInput = document.getElementById("lowercase");
+const numbersInput = document.getElementById("numbers");
+const symbolsInput = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate");
+let passwordLength, endResult, passwordCharSet;
+
+
+generateBtn.addEventListener('click', () => {
+	passwordLength = "", endResult = "", passwordCharSet = "";
+
+	if (uppercaseInput.checked) { passwordCharSet += uppercaseStr; }
+	if (lowercaseInput.checked) { passwordCharSet += lowercaseStr; }
+	if (numbersInput.checked) { passwordCharSet += numbersStr; }
+	if (symbolsInput.checked) { passwordCharSet += symbolstr; }
+
+	passwordLength = Number(passwordLengthInput.value);
+
+	for (let i = 0; i < passwordLength; i++) {
+		endResult += passwordCharSet.charAt(
+			Math.floor(Math.random() * passwordCharSet.length)
+		);
+	}
+
+	if (endResult == "" || Number(endResult.length) < 6) {
+
+		if (passwordLength < 6) {
+
+			alertMessage.innerHTML = "The minimum length for password 6!"
+			alert.classList.add('fail');
+
+			setTimeout(function () {
+				alert.classList.remove('fail');
+				result.value = "";
+				passwordLengthInput.value = "20";
+			}, 3000);
+
+		} else {
+
+			alertMessage.innerHTML = "Please select an option before generating a password!"
+			alert.classList.add('fail');
+
+			setTimeout(function () {
+				alert.classList.remove('fail');
+				result.value = "";
+				uppercaseInput.checked = "checked";
+				lowercaseInput.checked = "checked";
+				numbersInput.checked = "checked";
+				symbolsInput.checked = "checked";
+			}, 3000);
+		}
+
+	} else {
+		result.value = endResult;
+	}
+
+});
+
+copyBtn.addEventListener('click', () => {
+	result.select();
+	result.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	alertMessage.innerHTML = "Copied to Clipboard!"
+	alert.classList.add('success');
+	setTimeout(function () {
+		alert.classList.remove('success');
+	}, 3000);
+});
 
 /*******************************/
 
-const output = document.querySelector('.screen .output');
-const copy = document.getElementById('copy');
-const passwordLength = document.getElementById('passlength');
-const upercase = document.getElementById('upercase');
-const lowercase = document.getElementById('lowercase');
-const numbers = document.getElementById('numbers');
-const symbols = document.getElementById('symbols');
-const generate = document.getElementById('generate');
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function () {
+		navigator.serviceWorker.register('/sw.js').then(function (registration) {
+			// Registration was successful
+			console.log('ServiceWorker registration successful with scope: ', registration
+				.scope);
+		}, function (err) {
+			// registration failed :(
+			console.log('ServiceWorker registration failed: ', err);
+		});
+	});
+}
 
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
 
-let symbolstr = '&_)$*%^!=-/<>(@``ç#|@\)=&';
-let upercaseStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZGDJD';
-let lowercaseStr = 'abcdefghijklmnopqrstuvwxyzhgsfd';
-let numbersStr = '0123456789635694378265487';
+window.addEventListener('beforeinstallprompt', (e) => {
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+	// Update UI to notify the user they can add to home screen
+	addBtn.style.display = 'block';
 
-generate.addEventListener('click', () => {
-
-	if (upercase.checked && lowercase.checked && symbols.checked && numbers.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = '&hA5$jL9@sB5_mP1&nV6)cR5L*gF32';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-
-	} else if (upercase.checked && lowercase.checked && numbers.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = 'hA5jL9sB5mP1nV6cR5LgF32Lo5Fd18';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-
-	} else if (symbols.checked && numbers.checked && lowercase.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = '&h5$j9@s5_m1&n6)c5*g32)5l@9p#0';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-
-	} else if (upercase.checked && lowercase.checked && symbols.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = 'jY=Sn@Vt$&hA$jL@sB_mP&nV)cRL*gF';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-	} else if (upercase.checked && lowercase.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = 'hAjLsBmPnVcRLgFkGvEjIgOpScTnY';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-	} else if (symbols.checked && numbers.checked) {
-		let random = Math.floor(Math.random() * 10);
-		let allStr = '&5$9@5_1&6)5*32@4\8=9!5<0#3}0+';
-		output.value = allStr.slice(random, random + +passwordLength.value);
-	} else if (symbols.checked) {
-		let random = Math.floor(Math.random() * 5);
-		output.value = symbolstr.slice(random, random + +passwordLength.value);
-	} else if (numbers.checked) {
-		let random = Math.floor(Math.random() * 5);
-		output.value = numbersStr.slice(random, random + +passwordLength.value);
-	} else if (lowercase.checked) {
-		let random = Math.floor(Math.random() * 10);
-		output.value = lowercaseStr.slice(random, random + +passwordLength.value);
-	} else if (upercase.checked) {
-		let random = Math.floor(Math.random() * 10);
-		output.value = upercaseStr.slice(random, random + +passwordLength.value);
-	}
+	addBtn.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+	});
 });
 
-// Ability to copy the Text.
-function copyText() {
-	output.select();
-	output.setSelectionRange(0, 99999)
-	document.execCommand("copy");
-	alert("Text has been copied to clipboard.");
-}
+window.addEventListener("beforeinstallprompt", function (beforeInstallPromptEvent) {
+	beforeInstallPromptEvent.preventDefault(); // Prevents immediate prompt display
+
+	// Shows prompt after a user clicks an "install" button
+	installButton.addEventListener("click", function (mouseEvent) {
+		// you should not use the MouseEvent here, obviously
+		beforeInstallPromptEvent.prompt();
+	});
+
+	installButton.hidden = false; // Make button operable
+});
